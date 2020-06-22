6e4b054a4a52c2bb027cca134d8a34c2bf17ba44
Noobie <noobie@localhost.localdomain>
2020-06-22 16:04:37 +0700

    new

---
+++ messenger.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+int main();
+void menu();
+struct User{
+    string name;
+    string sent[50];
+    string inbox[50];
+};
+class Messeger
+{
+    private:
+        User *user;
+        int n;
+    public:
+
+      void Register()
+      {
+           cout << "Input User:" ;
+           cin >> n;
+           user = new User[n];
+           for(size_t i =0 ;i<n;i++)
+           {
+               cout << "User["<<i+1<<"] "<<"Name:";
+               cin >> user[i].name;
+           }
+      }
+      void showUser()
+      {
+            for(int i =0 ;i<n;i++)
+            {
+               cout << "User[" << i+1 << "] " << "Name:";
+               cout << user[i].name << endl;
+            }
+            cout << "Press any to continue!" << endl;
+      }
+};
+
+void menu()
+{
+
+     int x;
+
+    Messeger user;
+    system("clear");
+    cout << "1.Register" << endl;
+    cout << "2.Show all users" << endl;
+    cout << "3.See the conversations" << endl;
+    cout << ":";cin >> x;
+    switch(x){
+        case 1:user.Register();menu();
+        case 2:user.showUser();
+        case 0:break;
+        default:menu();
+    }
+}
+
+int main()
+{
+    menu();
+    return 0;
+}
